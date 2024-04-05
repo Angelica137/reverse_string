57b4c6d586b1a3a957060689dc965bb68b1236b3
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 12:26:32 +0100

    all tests pass

--- tests/test_reverse_word.py
+++ tests/test_reverse_word.py
@@ -1,8 +1,14 @@
 from src.reverse_word import reverse_string
 
 
-def test_reverse_word_returns_reversed_wrods_as_req():
-    """Tests revers_word returns list of words
-        in the og string"""
+def test_reverse_word_returns_reversed_wrods_as_req_short_words():
+    """Tests revers_word returns given string when there
+        are no long words"""
     string = "This is a test"
     assert reverse_string(string) == "This is a test"
+
+
+def test_reverse_word_returns_reversed_wrods_as_req_long_words():
+    """Tests revers_word returns a string with reversed words if long"""
+    string = "Hey fellow warriors"
+    assert reverse_string(string) == "Hey wollef sroirraw"
