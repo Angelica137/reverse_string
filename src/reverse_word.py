427e9b207bc8eacc3537c7a68b4fb70b585a6ace
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 12:25:00 +0100

    reverse_word returns the given string if not long words, green

--- src/reverse_word.py
+++ src/reverse_word.py
@@ -1,3 +1,10 @@
 def reverse_string(sentence) -> str:
     words = sentence.split(" ")
-    return words
+    new_sentence = ""
+    for word in words:
+        if len(word) < 5:
+            new_sentence += word + " "
+        else:
+            reverse_word = word[::-1]
+            new_sentence += reverse_word + " "
+    return new_sentence.rstrip()
--- tests/test_reverse_word.py
+++ tests/test_reverse_word.py
@@ -1,8 +1,8 @@
 from src.reverse_word import reverse_string
 
 
-def test_reverse_word_returns_list_of_words():
+def test_reverse_word_returns_reversed_wrods_as_req():
     """Tests revers_word returns list of words
         in the og string"""
     string = "This is a test"
-    assert reverse_string(string) == ["This", "is", "a", "test"]
+    assert reverse_string(string) == "This is a test"
