5166163cdfd48cb1e8c16a47199416b24d2402f2
Angelica <Angelica137@users.noreply.github.com>
2024-04-05 12:14:14 +0100

    reverse_string returns list, red

--- tests/test_reverse_word.py
+++ tests/test_reverse_word.py
@@ -1,7 +1,8 @@
-from src.reverse_word import *
+from src.reverse_word import reverse_string
 
 
-def test_reverse_word_returns_og_string():
-    """Tests revers_word returns og strng"""
+def test_reverse_word_returns_list_of_words():
+    """Tests revers_word returns list of words
+        in the og string"""
     string = "This is a test"
-    assert reverse_string(string) == "This is a test"
+    assert reverse_string(string) == ["This", "is", "a", "test"]
